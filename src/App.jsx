2f9ac4b5e777ab4a3a9abe47c0dff2ea9c075750
Pedrooaj <pedroantonio5735@gmail.com>
2024-06-20 12:36:20 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 import { Form } from './Components/Form/Form'
 export const App = () => {
   return (
-    <>
+    <div className='app'>
     <Form />
     <footer>Desenvolvido por Pedrooaj &copy;</footer>
-    </>
+    </div>
   )
 }
 
